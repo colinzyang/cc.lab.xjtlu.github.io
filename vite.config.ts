25f25e6ec781e49f206e0ef0dc04c12f50f7531a
colinzyang <colin.zhicheng.yang@gmail.com>
2026-01-23 15:21:30 +0800

    feat: 提供netlify服务

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cc.lab.xjtlu.github.io/', // Project pages subdirectory deployment
+  base: '/', // Netlify root directory deployment (use '/cc.lab.xjtlu.github.io/' for GitHub Pages)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
